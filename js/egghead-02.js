d28273cc322cb2e70da58c26d00ce800b90fc39e
Miguel Angel <miguelangel@vasava.es>
2016-02-08 15:08:53 +0100

    finish lesson 2

--- js/egghead-02.js
+++ js/egghead-02.js
@@ -1,8 +1,24 @@
 import Rx from 'rx';
 
-Rx.Observable.timer(0, 1000)
-.map(i => `aaa Seconds elapsed ${i}`)
-.subscribe(text => {
-    const container = document.querySelector('#app');
-    container.textContent = text;
-})
+function main(){
+    return Rx.Observable.timer(0, 1000)
+        .map(i => `Seconds elapsed ${i}`)
+}
+
+function DOMEffect (text$){
+    text$.subscribe(text => {
+        const container = document.querySelector('#app');
+        container.textContent = text;
+    })
+}
+
+function consoleLogffect (msg$){
+    msg$.subscribe(msg => {
+        console.log(msg);
+    })
+}
+
+const sink = main();
+
+DOMEffect(sink)
+consoleLogffect(sink)
